fa465589b97332a36c4189b72e74417d19ed6456
anshul <anshul.punia001@gmail.com>
2026-08-13 10:24:59 +0530

    intial commit

---
+++ celsioustofahreniet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    float c,f ;
+    printf("enter temp. in c ");
+    scanf("%f",&c);
+        f = (c*9.0/5.0 +32.0);
+
+    printf("temp   in faherniet is :%2.f\n",f );
+    return 0;
+    
+
+
+
+
+}
