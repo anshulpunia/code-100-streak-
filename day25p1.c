e0869d8565b3cd042952224c8e376d7eee21fc33
anshul <anshul.punia001@gmail.com>
2026-09-03 23:00:54 +0530

    day 25 cmp

---
+++ day25p1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+    for (int i=5;i>0;i--){
+    int a=i;
+        for(int j=5;j>=i;j--){
+            
+       printf("%d",a);
+       a++;
+
+       }
+       printf("\n");
+
+    }
+
+
+}
---
+++ day25p2.c
@@ -0,0 +1,27 @@
+// Write a program to print the following pattern:
+// *****
+//  ****
+//   ***
+//    **
+//     *
+#include <stdio.h>
+int main (){
+    int i,j; 
+    for(i=5;i>0;i--){
+
+        for(j=i;j>0;j--){
+            printf("*");
+        }
+
+        printf("\n");
+        for(int a = 5;a>=i;a--){
+            printf(" ");
+
+        }
+
+
+    }
+
+
+return 0 ;
+}
