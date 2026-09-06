0f49d692c139a81c26ef227add46aa1534b9f389
anshul <anshul.punia001@gmail.com>
2026-09-06 23:50:05 +0530

    day 28 comp

---
+++ day28p1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (){
+   int b,a,n ;
+   printf("enter the no.",&n);
+   scanf("%d",&n);
+   for(a=1;a<=n;a++){
+    for(b=1;b<=n;b/a!=0){
+        printf("%d",b);
+    }
+
+   } 
+    
+    
+    
+    return 0;
+
+}
