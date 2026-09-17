3dbf4ca5d18385f59ffd5fde563e6a352112b74c
anshul <anshul.punia001@gmail.com>
2026-09-17 23:46:47 +0530

    day 39 comp

---
+++ day39p2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    printf("Enter rows and columns: \n");
+    scanf("%d %d", &r, &c);
+
+    int matrix[r][c];
+    printf("enter the matrix\n");
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }2 
+
+    int diagonal_sum = 0;
+    for (int i = 0; i < r && i < c; i++)
+    {
+        diagonal_sum += matrix[i][i];
+    }
+
+    printf("sum of diagonal is %d\n", diagonal_sum);
+}
