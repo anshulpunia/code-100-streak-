4c3517be25e49c473c34caa3e2a345d3cd103417
anshul <anshul.punia001@gmail.com>
2026-09-26 23:45:16 +0530

    day 47 comp

---
+++ day47p1.c
@@ -0,0 +1,28 @@
+// Q89: Count frequency of a given character in a string.
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char st[100];
+    char target;
+    int count = 0;
+
+    printf("Enter the string: ");
+    if (fgets(st, sizeof(st), stdin) == NULL) {
+        return 1;
+    }
+
+    printf("Enter the character to find: ");
+    if (scanf(" %c", &target) != 1) {
+        return 1;
+    }
+
+    for (int i = 0; st[i] != '\0'; i++) {
+        if (st[i] == target) {
+            count++;
+        }
+    }
+
+    printf("Occurrences of '%c': %d\n", target, count);
+    return 0;
+}
